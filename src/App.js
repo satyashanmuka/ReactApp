279887b10cbb82289060ffac2cc5eacb4d996ee2
Satya Kundeti <shanmuka45@gmail.com>
2018-04-14 18:30:16 -0500

    resolved compilation issues

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import main from './assets/images/main_img.jpg';
 
 class App extends Component {
   render() {
@@ -8,9 +9,9 @@ class App extends Component {
 
 <div className="container">
 
-  
 
       <div className="row about_row">
+        <img className="desktop_hd" src={main}></img>
         <div className="col-md-9 col-sm-9 col-xs-9 col-12 abt_right">
           <p>Sainath was born and brought up in Vanasthalipuram, Hyderabad.<br/><br/>
               He always believed that he is capable of doing things in his life which have great purpose to it. With a strong belief and conviction, he could convert every challenge into opportunities and succeeded everywhere. Very soon he realized the secret ingredient for his success was self-awareness or understanding one’s own self and loving it. One of his key forte was in management and it was also his dream to be an entrepreneur.  Rigorous hard work and pursuance earned him management degree at Indian Institute of Management Calcutta which he put into practice later by engaging with start-ups. Working with early stage start-ups helped him grow in multiple areas of business most importantly set-up and maintaining fully operational business from the scratch or almost nothing.  His true calling was in business leadership. <br/><br/>
