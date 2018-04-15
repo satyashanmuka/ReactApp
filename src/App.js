0f081831592f6f94a005c44d286fcf56d8fccb65
Satya Kundeti <shanmuka45@gmail.com>
2018-04-15 03:03:20 -0500

    first set of working changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import mobile_header from './assets/images/mobile_header.png';
 import logo from './assets/images/logo.png';
 import logo_mobile from './assets/images/logo_mobile.png';
 import abt_title from './assets/images/abt_title.jpg';
+import footer_logo from './assets/images/footer_logo.png';
+import footer_fb from './assets/images/footer_fb.png';
+import footer_linkedin from './assets/images/footer_linkedin.png';
+import about_img from './assets/images/about_img.jpg';
+import abt_fb from './assets/images/abt_fb.jpg';
+import abt_twitter from './assets/images/abt_twitter.jpg';
+import abt_linkedin from './assets/images/abt_linkedin.jpg';
 import * as ReactBootstrap from 'react-bootstrap';
 
 class App extends Component {
@@ -39,6 +46,43 @@ class App extends Component {
           <h2><img className="title_left" src={abt_title} /> Watch Live <img className="title_left" src={abt_title} /></h2>
           <iframe width="660" height="415" src="https://www.youtube.com/embed/ojvYcumBWzw" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
         </div>
+        <section id="more" class="row">
+          {/*<!-- about row starts here -->*/}
+            <div class="row about_row">
+ 
+              <h2><img class="title_left" src={abt_title} />About the founder<img class="title_right" src={abt_title} /></h2>
+ 
+              <div class="col-md-3 col-sm-3 col-xs-3 col-12 abt_left">
+                <img class="profile_img" src={about_img} alt="Strong Human Network" />
+                <div class="abt_social_media">
+                  <a href="https://www.facebook.com/sainathmaratha" target="_blank"><img src={abt_fb} alt="Facebook" /></a>
+                  <a href="https://twitter.com/itsimplyme" target="_blank"><img src={abt_twitter} alt="Twitter" /></a>
+                  <a href="https://www.linkedin.com/in/sainathm/" target="_blank"><img src={abt_linkedin} alt="Linkedin" /></a>
+                </div>
+              </div>
+ 
+              <div class="col-md-9 col-sm-9 col-xs-9 col-12 abt_right">
+                <p>Sainath was born and brought up in Vanasthalipuram, Hyderabad.<br/><br/>
+                  He always believed that he is capable of doing things in his life which have great purpose to it. With a strong belief and conviction, he could convert every challenge into opportunities and succeeded everywhere. Very soon he realized the secret ingredient for his success was self-awareness or understanding one’s own self and loving it. One of his key forte was in management and it was also his dream to be an entrepreneur.  Rigorous hard work and pursuance earned him management degree at Indian Institute of Management Calcutta which he put into practice later by engaging with start-ups. Working with early stage start-ups helped him grow in multiple areas of business most importantly set-up and maintaining fully operational business from the scratch or almost nothing.  His true calling was in business leadership. <br/><br/>
+                  At Paytm he was leading Telangana and Andhra Pradesh upcountry during demonetization, also handled large decentralized teams with an ease. Later he realized that his success as a leader was mainly because he could understand people behavior and manage them well. Though the new mantra of success has its roots in the old one: understanding of self. Self-awareness has great impact on individuals, businesses, relationships and the whole ecosystem where the world operates. With Business transformation happening worldwide and India at center of driving this global change people focused companies are need of the hour. Unfortunately, most of the brands are dividing people for market growth rather than unifying people under single brand. This led to genesis of StrongHumanNetwork (SHN).<br/><br/>
+                  As a founder of SHN, Sainath aspires to transform the whole world by transforming one human at a time.<br/><br/>
+                  <b>StrongHumanNetwork</b> aspires to unite the human race.
+                </p>
+              </div>
+              </div>
+          {/*<!-- about row ends here -->*/}
+        </section>
+        {/*<!-- footer row starts here -->*/}
+        <div className="row footer_row">
+          <div className="row footer_top">
+            <div className="col-md-6 col-sm-6 col-xs-6 col-6 ft_left"><img src={footer_logo} alt="Strong Human Network" /></div>
+              <div className="col-md-6 col-sm-6 col-xs-6 col-6 ft_right"><span className="connect_text">Connect with us: </span><a className="footer_fb_icon" href="https://www.facebook.com/Stronghumannetwork/" target="_blank"><img src={footer_fb} alt="Facebook" /></a><a href="https://www.linkedin.com/company/13662438/" target="_blank"><img src={footer_linkedin} alt="Linkedin" /></a></div>
+            </div>
+            <div className="row copyrights">
+            <div className="col-md-12">&copy; 2018. Shn.world</div>
+          </div>
+        </div>
+      {/*<!-- footer row ends here -->*/}
       </div>
     );
   }
